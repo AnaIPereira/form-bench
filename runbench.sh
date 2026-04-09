e21c8ef6a308cae7b84829d26044a971acd8a028
AnaIPereira <ana.pereira98@icloud.com>
2026-04-09 11:06:17 +0100

    change memory, time, delete 2 and 4 threads and delete mass-fact

--- runbench.sh
+++ runbench.sh
@@ -8,27 +8,26 @@
 #SBATCH --output=log/logfile-%A-%a.log 
 
 #SBATCH --export=ALL
-#SBATCH --mem=20G
+#SBATCH --mem=40G
 #SBATCH --cpus-per-task=64
-#SBATCH --time 0-10:00:00
+#SBATCH --time 0-20:00:00
 #SBATCH --mail-type=END # notifications 
 #SBATCH --mail-user=ana.costa-pereira@liverpool.ac.uk
 
-#SBATCH --array=1-9
+#SBATCH --array=1-8
 case $SLURM_ARRAY_TASK_ID in
        1) TESTS="mincer" ;;
        2) TESTS="minceex" ;;
-       3) TESTS="mass-fact" ;;
-       4) TESTS="forcer" ;;
-       5) TESTS="forcer-exp" ;;
-       6) TESTS="fmft" ;;
-       7) TESTS="mbox1l" ;;
-       8) TESTS="color" ;;
-       9) TESTS="chromatic" ;;
+       3) TESTS="forcer" ;;
+       4) TESTS="forcer-exp" ;;
+       5) TESTS="fmft" ;;
+       6) TESTS="mbox1l" ;;
+       7) TESTS="color" ;;
+       8) TESTS="chromatic" ;;
 esac       
 
 #running for array
-./run-compare.sh --tests="$TESTS" --label="testsAna_64_difficulty=2" --timestamp="1" --testdir="$TMPDIR"
+./run-compare.sh --tests="$TESTS" --label="testsAna_64_difficulty=2_40G" --timestamp="1" --testdir="$TMPDIR"
 
 #if running for one test
 #./run-compare.sh \
