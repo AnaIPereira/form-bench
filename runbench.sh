687a3bf5eab330242b49cf6fcb94bd1f64fc8142
AnaIPereira <ana.pereira98@icloud.com>
2026-04-16 10:44:32 +0100

    res

--- runbench.sh
+++ runbench.sh
@@ -9,31 +9,28 @@
 
 #SBATCH --export=ALL
 #SBATCH --mem=40G
-#SBATCH --cpus-per-task=32
-#SBATCH --time 0-40:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --time 0-10:00:00
 #SBATCH --mail-type=END # notifications 
 #SBATCH --mail-user=ana.costa-pereira@liverpool.ac.uk
 
 #SBATCH --array=1-8
 case $SLURM_ARRAY_TASK_ID in
-       1) TESTS="mincer" ;;
-       2) TESTS="minceex" ;;
-       3) TESTS="forcer" ;;
-       4) TESTS="forcer-exp" ;;
-       5) TESTS="fmft" ;;
-       6) TESTS="mbox1l" ;;
-       7) TESTS="color" ;;
-       8) TESTS="chromatic" ;;
+       1) TESTS="trace" ;;
+       2) TESTS="mincer" ;;
 esac       
 
 #running for array
-./run-compare.sh --tests="$TESTS" --label="testsAna_32_difficulty=3_40G_40h" --timestamp="1" --testdir="$TMPDIR"
+#./run-compare.sh --tests="$TESTS" --label="testsAna_32_difficulty=3_40G_40h" --timestamp="1" --testdir="$TMPDIR"
 
-#if running for one test
-#./run-compare.sh \
-#--tests="trace" \
-#--label="testsAnatrace" \
-#--timestamp="1" \
-#--testdir="$TMP"
+OUT=perf-${SLURM_ARRAY_TASK_ID}
+
+perf record -g -o ${OUT}.data \
+./run-compare.sh --tests="$TESTS" \
+--label="testsAna_16_difficulty=1_40G_10h_N=1_perf" \
+--timestamp="1" \
+--testdir="$TMPDIR"
+
+perf report --stdio -i ${OUT}.data > ${OUT}.txt
 
 
