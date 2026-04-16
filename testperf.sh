e1724e62189a30464c7ca46809475608687aa09c
AnaIPereira <ana.pereira98@icloud.com>
2026-04-16 11:08:09 +0100

    res

--- testperf.sh
+++ testperf.sh
@@ -3,6 +3,8 @@
 #SBATCH -A dr004
 #SBATCH --job-name=perf_test
 #SBATCH --time=00:05:00
+#SBATCH --mail-type=END # notifications 
+#SBATCH --mail-user=ana.costa-pereira@liverpool.ac.uk
 
 echo "PATH=$PATH"
 which perf
