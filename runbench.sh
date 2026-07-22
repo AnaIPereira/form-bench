ce999feb571473de49d77ce147c666c07d984c2e
AnaIPereira <ana.pereira98@icloud.com>
2026-07-22 10:09:10 +0100

    run benchmarks for new sortbot

--- runbench.sh
+++ runbench.sh
@@ -9,43 +9,22 @@
 
 #SBATCH --export=ALL
 #SBATCH --mem=20G
-#SBATCH --cpus-per-task=64
-#SBATCH --time 0-5:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --time 0-4:00:00
 #SBATCH --mail-type=END # notifications 
 #SBATCH --mail-user=ana.costa-pereira@liverpool.ac.uk
 
-#SBATCH --array=1-11
+#SBATCH --array=1-3
 
 case $SLURM_ARRAY_TASK_ID in
-       1) TESTS="mincer" ;;
-       2) TESTS="minceex" ;;
-       3) TESTS="forcer" ;;
-       4) TESTS="forcer-exp" ;;
-       5) TESTS="fmft" ;;
-       6) TESTS="mbox1l" ;;
-       7) TESTS="chromatic" ;;
-       8) TESTS="trace" ;;
-       9) TESTS="sort-small" ;;
-       10) TESTS="sort-large" ;;
-       11) TESTS="sort-disk" ;;
+       1) TESTS="sort-2-disk" ;;
+       2) TESTS="sort-2-large" ;;
+       3) TESTS="sort-2-small" ;;
 esac      
-
-
-
-
-#case $SLURM_ARRAY_TASK_ID in
- #      1) TESTS="nowork-10" ;;
-  #     2) TESTS="nowork-100" ;;
-   #    3) TESTS="ordered-10" ;;
-    #   4) TESTS="ordered-100" ;;
-     #  5) TESTS="disordered-10" ;;
-      # 6) TESTS="disordered-100" ;;
-#esac  
-
-     
+ 
 
 #running for array
-./run-compare.sh --tests="$TESTS" --label="FORMplotnt" --timestamp="1" --testdir="$TMPDIR" --form_cmds="tform8_1_1 -w4, tform8_1_1 -w8, tform8_1_1 -w16, tform8_1_1 -w32, tform8_1_1 -w64"
+./run-compare.sh --tests="$TESTS" --label="sotbotsfunc" --timestamp="1" --testdir="$TMPDIR" --form_cmds="tformsb -w8, tformsb -w16, tformsb -w32"
 
 
 
