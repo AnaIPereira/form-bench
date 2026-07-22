bad87592aff9053fa3589b583150618f6c5c7c93
AnaIPereira <ana.pereira98@icloud.com>
2026-07-22 14:05:27 +0100

    runbench 32

--- runbench.sh
+++ runbench.sh
@@ -9,7 +9,7 @@
 
 #SBATCH --export=ALL
 #SBATCH --mem=20G
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 #SBATCH --time 0-2:00:00
 #SBATCH --mail-type=END # notifications 
 #SBATCH --mail-user=ana.costa-pereira@liverpool.ac.uk
@@ -26,10 +26,10 @@ esac
 #running for array
 ./run-compare.sh \
   --tests "$TESTS" \
-  --label "sortbotsfunc16" \
+  --label "sortbotsfunc32" \
   --timestamp "1" \
   --testdir "$TMPDIR" \
-  --form_cmds "tformsb -w16,tform10_10_1 -w16"
+  --form_cmds "tformsb -w32,tform10_10_1 -w32"
 
 
 
