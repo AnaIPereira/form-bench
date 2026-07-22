789eecdd1a3370ac4aa8ecf18ab01e09463242bf
AnaIPereira <ana.pereira98@icloud.com>
2026-07-22 13:55:29 +0100

    run sb 8

--- runbenchcluster.sh
+++ runbenchcluster.sh
@@ -7,7 +7,7 @@
 
 #SBATCH --export=ALL
 #SBATCH --mem=20G
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=8
 #SBATCH --time 0-2:00:00
 #SBATCH --mail-type=END # notifications 
 #SBATCH --mail-user=ana.costa-pereira@liverpool.ac.uk
@@ -22,13 +22,13 @@ esac
  
 export LD_LIBRARY_PATH="$HOME/software/lib:$LD_LIBRARY_PATH"
 
-#running for array
+
 ./run-compare.sh \
-  --tests="$TESTS" \
-  --label="sortbotsfunc64" \
-  --timestamp="1" \
-  --testdir="$TMPDIR" \
-  --form_cmds="$HOME/software/bin/tformsb -w64, $HOME/software/bin/tform5.0 -w64"
+  --tests "$TESTS" \
+  --label "sortbotsfunc8" \
+  --timestamp "1" \
+  --testdir "$TMPDIR" \
+  --form_cmds "$HOME/software/bin/tformsb -w8,$HOME/software/bin/tform5.0 -w8"
 
 
 
