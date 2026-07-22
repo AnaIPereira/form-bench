5904d57125e1d19bb75ade670bdc4a8b6ac34922
AnaIPereira <ana.pereira98@icloud.com>
2026-07-22 13:19:48 +0100

    run64

--- runbench.sh
+++ runbench.sh
@@ -9,7 +9,7 @@
 
 #SBATCH --export=ALL
 #SBATCH --mem=20G
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=64
 #SBATCH --time 0-2:00:00
 #SBATCH --mail-type=END # notifications 
 #SBATCH --mail-user=ana.costa-pereira@liverpool.ac.uk
@@ -24,7 +24,7 @@ esac
  
 
 #running for array
-./run-compare.sh --tests="$TESTS" --label="sotbotsfunc32" --timestamp="1" --testdir="$TMPDIR" --form_cmds="tformsb -w32, tform10_10_1 -w32"
+./run-compare.sh --tests="$TESTS" --label="sotbotsfunc64" --timestamp="1" --testdir="$TMPDIR" --form_cmds="tformsb -w64, tform10_10_1 -w64"
 
 
 
