da47a18de69c5a511dbe4a6c94e7738c19be4346
AnaIPereira <ana.pereira98@icloud.com>
2026-07-23 12:19:47 +0100

    run 64

--- runbenchcluster.sh
+++ runbenchcluster.sh
@@ -7,7 +7,7 @@
 
 #SBATCH --export=ALL
 #SBATCH --mem=20G
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=64
 #SBATCH --time 0-3:00:00
 #SBATCH --mail-type=END # notifications 
 #SBATCH --mail-user=ana.costa-pereira@liverpool.ac.uk
@@ -26,11 +26,11 @@ export LD_LIBRARY_PATH="$HOME/software/lib:$LD_LIBRARY_PATH"
 
 ./run-compare.sh \
   --tests "$TESTS" \
-  --label "sortbotsfunc32" \
+  --label "sortbotsfunc64" \
   --timestamp "1" \
   --testdir "$TMPDIR" \
   --runs 30 \
-  --form_cmds "$HOME/software/bin/tformsortbot -w32,$HOME/software/bin/tformformmaster -w32"
+  --form_cmds "$HOME/software/bin/tformsortbot -w64,$HOME/software/bin/tformformmaster -w64"
 
 
 
